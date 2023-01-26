b21207a5b3ae664cdc96c0420de2b5d4377e70bd
Harshit Bargali <harshitbargali567@gmail.com>
2023-01-26 12:04:24 +0530

    Order controller added and razorpay utility added

--- routes/order.js
+++ routes/order.js
@@ -1,30 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const Razorpay = require('razorpay')
-const Shop = require('../model/shop');
 
-const instance = new Razorpay({
-    key_id: process.env.KEY_ID,
-    key_secret: process.env.KEY_SECRET,
-});
+const orderController = require('../controllers/order')
 
 router.route('/:id').
-get(async (req, res)=>{
-    const {id} = req.params;
-    const product = await Shop.findById(id);
-    const options = {
-        amount:product.price,
-        currency:"INR",
-        receipt:"order123",
-        notes:{name:product.name,
-            desc:product.description}
-    };
-    instance.orders.create(options, (error, order)=>{
-        if(error){
-            res.status(500).json(error);
-        }
-        res.status(200).json(order);
-    } )
-})
+get(orderController.getOrder)
 
 module.exports = router;
